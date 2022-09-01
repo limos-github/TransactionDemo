5550de4e8ce3c88006d4f6c06510803243443eea
libiao <libiao@jtexpress.com.cn>
2022-09-01 11:36:38 +0800

    1. 修改自动生成代码类型

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # TransactionDemo
 SPRING BOOT + ORACLE + MYBATIS PLUS 
+
+
+`-- auto-generated definition
+create table TEST_TRANSACTION
+(
+ID     NUMBER(20) not null
+constraint TEST_TRANSACTION_PK
+primary key,
+LENGTH NUMBER(20),
+WIDTH  NUMBER(20),
+HEIGHT NUMBER(20),
+WEIGHT NUMBER(20)
+);
+comment on table TEST_TRANSACTION is '测试事务';
+comment on column TEST_TRANSACTION.LENGTH is '长';
+comment on column TEST_TRANSACTION.WIDTH is '宽';
+comment on column TEST_TRANSACTION.HEIGHT is '高';
+comment on column TEST_TRANSACTION.WEIGHT is '重量';
+索引
+CREATE SEQUENCE TEST_TRANSACTION_SEQ
+START WITH 1
+INCREMENT BY 1
+MINVALUE 1 NOMAXVALUE
+cache 30;
+`
--- src/main/java/com/limo/transactiondemo/entity/TestTransaction.java
+++ src/main/java/com/limo/transactiondemo/entity/TestTransaction.java
@@ -1,6 +1,6 @@
 package com.limo.transactiondemo.entity;
 
-import java.math.BigDecimal;
+import com.baomidou.mybatisplus.annotation.KeySequence;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableField;
@@ -13,42 +13,50 @@ import lombok.EqualsAndHashCode;
  * 测试事务
  * </p>
  *
+ *
+ * CREATE SEQUENCE TEST_TRANSACTION_SEQ
+ *     START WITH 1
+ *     INCREMENT BY 1
+ *     MINVALUE 1 NOMAXVALUE
+ *     cache 30;
+ *
  * @author zyy
  * @since 2022-09-01
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
 @TableName("TEST_TRANSACTION")
+@KeySequence("TEST_TRANSACTION_SEQ")
 public class TestTransaction implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @TableId("ID")
-    private BigDecimal id;
+    private Long id;
 
     /**
      * 长
      */
     @TableField("LENGTH")
-    private BigDecimal length;
+    private Long length;
 
     /**
      * 宽
      */
     @TableField("WIDTH")
-    private BigDecimal width;
+    private Long width;
 
     /**
      * 高
      */
     @TableField("HEIGHT")
-    private BigDecimal height;
+    private Long height;
 
     /**
      * 重量
      */
     @TableField("WEIGHT")
-    private BigDecimal weight;
+    private Long weight;
 
 
 }
